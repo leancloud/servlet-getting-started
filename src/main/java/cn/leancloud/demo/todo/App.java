171db31655ef05aed2f94a2c249278a9d3689ef3
lbt05 <lbt0506@gmail.com>
2016-07-01 17:27:25 +0800

    first commit to add pom dependence

---
+++ src/main/java/cn/leancloud/demo/todo/App.java
@@ -0,0 +1,13 @@
+package cn.leancloud.demo.todo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
