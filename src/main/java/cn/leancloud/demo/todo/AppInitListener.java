62ce2abefd2c54170ec6774c87ef2368f608d98d
wchen <sdjcw13@gmail.com>
2017-11-15 15:13:57 +0800

    升级 leanengine 中间件版本和一些细节调整

--- src/main/java/cn/leancloud/demo/todo/AppInitListener.java
+++ src/main/java/cn/leancloud/demo/todo/AppInitListener.java
@@ -7,6 +7,8 @@ import javax.servlet.annotation.WebListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.avos.avoscloud.AVCloud;
+import com.avos.avoscloud.AVOSCloud;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.internal.impl.JavaRequestSignImplementation;
 
@@ -20,6 +22,8 @@ public class AppInitListener implements ServletContextListener {
   private String appId = System.getenv("LEANCLOUD_APP_ID");
   private String appKey = System.getenv("LEANCLOUD_APP_KEY");
   private String appMasterKey = System.getenv("LEANCLOUD_APP_MASTER_KEY");
+  private String appEnv = System.getenv("LEANCLOUD_APP_ENV");
+  private String haveStaging = System.getenv("LEAN_CLI_HAVE_STAGING");
 
   @Override
   public void contextDestroyed(ServletContextEvent arg0) {}
@@ -29,18 +33,17 @@ public class AppInitListener implements ServletContextListener {
     logger.info("LeanEngine app init.");
     // 注册子类化
     AVObject.registerSubclass(Todo.class);
+
+    if ("development".equals(appEnv) && "true".equals(haveStaging) || "stage".equals(appEnv)) {
+      AVCloud.setProductionMode(false);
+    }
     // 初始化AVOSCloud，请保证在整个项目中间只初始化一次
     LeanEngine.initialize(appId, appKey, appMasterKey);
     // 在请求签名中使用masterKey以激活云代码的最高权限
     JavaRequestSignImplementation.instance().setUseMasterKey(true);
     // 打开 debug 日志
-    // AVOSCloud.setDebugLogEnabled(true);
+    AVOSCloud.setDebugLogEnabled(true);
     // 向云引擎注册云函数
     LeanEngine.register(Cloud.class);
-    if (System.getenv("LEANCLOUD_APP_ENV").equals("development")) {
-      // 如果是开发环境，则设置 AVCloud.callFunction 和 AVCloud.rpcFunction 调用本地云函数实现
-      // 如果需要本地开发时调用云端云函数实现，则注释掉下面语句。
-      LeanEngine.setLocalEngineCallEnabled(true);
-    }
   }
 }
