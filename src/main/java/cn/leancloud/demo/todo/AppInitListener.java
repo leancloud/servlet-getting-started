9ed11960503f4f6902fa1d1daad78e0bb3e3f7b0
jwfing <jwfing@gmail.com>
2018-05-25 14:59:15 +0800

    fixed compile error

--- src/main/java/cn/leancloud/demo/todo/AppInitListener.java
+++ src/main/java/cn/leancloud/demo/todo/AppInitListener.java
@@ -4,13 +4,15 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import cn.leancloud.AVLogger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.avos.avoscloud.AVCloud;
-import com.avos.avoscloud.AVOSCloud;
-import com.avos.avoscloud.AVObject;
-import com.avos.avoscloud.internal.impl.JavaRequestSignImplementation;
+import cn.leancloud.AVCloud;
+import cn.leancloud.core.AVOSCloud;
+import cn.leancloud.core.RequestSignImplementation;
+import cn.leancloud.AVObject;
+import cn.leancloud.impl.DefaultAVUserCookieSign;
 
 import cn.leancloud.LeanEngine;
 
@@ -35,14 +37,15 @@ public class AppInitListener implements ServletContextListener {
     AVObject.registerSubclass(Todo.class);
 
     if ("development".equals(appEnv) && "true".equals(haveStaging) || "stage".equals(appEnv)) {
-      AVCloud.setProductionMode(false);
+      AVOSCloud.setProductionMode(false);
     }
     // 初始化AVOSCloud，请保证在整个项目中间只初始化一次
     LeanEngine.initialize(appId, appKey, appMasterKey);
     // 在请求签名中使用masterKey以激活云代码的最高权限
-    JavaRequestSignImplementation.instance().setUseMasterKey(true);
+
+    RequestSignImplementation.setMasterKey(appMasterKey);
     // 打开 debug 日志
-    AVOSCloud.setDebugLogEnabled(false);
+    AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
     // 向云引擎注册云函数
     LeanEngine.register(Cloud.class);
   }
