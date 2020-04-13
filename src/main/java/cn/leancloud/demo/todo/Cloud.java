3a42f9efe451f8eb11f84d270ded7db55a49563a
jwfing <jwfing@gmail.com>
2020-04-13 17:53:15 +0800

    add sendSMS function fot testing

--- src/main/java/cn/leancloud/demo/todo/Cloud.java
+++ src/main/java/cn/leancloud/demo/todo/Cloud.java
@@ -2,6 +2,12 @@ package cn.leancloud.demo.todo;
 
 import cn.leancloud.EngineFunction;
 import cn.leancloud.EngineFunctionParam;
+import cn.leancloud.sms.AVSMS;
+import cn.leancloud.sms.AVSMSOption;
+import cn.leancloud.types.AVNull;
+import cn.leancloud.utils.StringUtil;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class Cloud {
 
@@ -13,4 +19,33 @@ public class Cloud {
     return String.format("Hello %s!", name);
   }
 
+  @EngineFunction("sendSMS")
+  public static String sendSMS(@EngineFunctionParam("mobile") String mobile) {
+    if (StringUtil.isEmpty(mobile)) {
+      return "What is your name?";
+    }
+    AVSMSOption option = new AVSMSOption();
+    AVSMS.requestSMSCodeInBackground(mobile, option).subscribe(new Observer<AVNull>() {
+      @Override
+      public void onSubscribe(Disposable disposable) {
+
+      }
+
+      @Override
+      public void onNext(AVNull avNull) {
+        ;
+      }
+
+      @Override
+      public void onError(Throwable throwable) {
+
+      }
+
+      @Override
+      public void onComplete() {
+
+      }
+    });
+    return "";
+  }
 }
