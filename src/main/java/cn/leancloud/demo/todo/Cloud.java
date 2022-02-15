56dca9170e05605cdca547333776919edf157aa8
jwfing <jwfing@gmail.com>
2022-02-15 20:54:31 +0800

    add sendPush cloud function

--- src/main/java/cn/leancloud/demo/todo/Cloud.java
+++ src/main/java/cn/leancloud/demo/todo/Cloud.java
@@ -1,14 +1,14 @@
 package cn.leancloud.demo.todo;
 
-import cn.leancloud.EngineFunction;
-import cn.leancloud.EngineFunctionParam;
-import cn.leancloud.EngineRequestContext;
+import cn.leancloud.*;
+import cn.leancloud.json.JSONObject;
 import cn.leancloud.sms.LCSMS;
 import cn.leancloud.sms.LCSMSOption;
 import cn.leancloud.types.LCNull;
 import cn.leancloud.utils.StringUtil;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import org.jetbrains.annotations.NotNull;
 
 public class Cloud {
 
@@ -44,6 +44,41 @@ public class Cloud {
       @Override
       public void onError(Throwable throwable) {
 
+      }
+
+      @Override
+      public void onComplete() {
+
+      }
+    });
+    return "";
+  }
+
+  @EngineFunction("sendPush")
+  public static String sendPush(@EngineFunctionParam("installationId") String installationId) {
+    if (StringUtil.isEmpty(installationId)) {
+      return "Illegal parameter: installationId is null.";
+    }
+    LCPush push = new LCPush();
+    LCQuery query = LCInstallation.getQuery();
+    query.whereEqualTo("installationId", installationId);
+    push.setQuery(query);
+    push.setMessage("test from LeanCloud Engine.");
+    push.sendInBackground().subscribe(new Observer<JSONObject>() {
+      @Override
+      public void onSubscribe(@NotNull Disposable disposable) {
+
+      }
+
+      @Override
+      public void onNext(@NotNull JSONObject jsonObject) {
+        System.out.println("succeed to sent push with installationId: " + installationId);
+      }
+
+      @Override
+      public void onError(@NotNull Throwable throwable) {
+        System.out.println("failed to sent push with installationId: " + installationId);
+        throwable.printStackTrace();
       }
 
       @Override
